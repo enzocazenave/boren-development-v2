97dd2515c05499851ad7183baeeda521fa0c5298
Enzo Cazenave <encaze24@hotmail.com>
2022-12-05 23:53:25 -0300

    change

--- src/components/MainPage/Features.jsx
+++ src/components/MainPage/Features.jsx
@@ -11,7 +11,7 @@ export const Features = () => {
                 <FeatureColumn icon={ <Brain width={60} /> } title="Diseño" text="Si puede explicarlo e imaginarlo, nosotros podemos crearlo." />
                 <FeatureColumn icon={ <Diamond width={60} /> } title="Mejor calidad" text="Garantizamos tu satisfacción." />
                 <FeatureColumn icon={ <Clock width={60} /> } title="Soporte 24/7" text="Nuestro soporte telefónico está Siempre disponible para usted." />
-                <FeatureColumn icon={ <Trophy width={60} /> } title="Desarrollo rápido" text="Tiempos de entrega rápidos, para que disfrutes de nuestro producto lo antes posible." />
+                <FeatureColumn icon={ <Trophy width={60} /> } title="Entregas" text="Tiempos acordes a cada proyecto y previamente pactado con el cliente." />
             </div>
         </div>
     )
